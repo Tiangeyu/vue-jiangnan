9c4f46e447d859e2ed6d282711d8ec3882692fd6
tiangeyu <tiangeyu@zhiyun88.com>
2021-08-16 15:27:15 +0800

    <优化>(注释)：添加注释

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,8 @@ import {setItem, getItem} from './assets/js/common'
 export default {
   name: 'app',
   created() {
+
+    // 测试封版111
      this.$http.get('/eb-web-api/mobile/base/queryPicUrl').then((res)=>{
         setItem('picUrl', res.body.url)
         this.$router.push("/index")
